0b005aa4529bf97b3a49792302568862ebaa1b9a
Stuart Simmons <stuart.simmons0@gmail.com>
2015-01-06 08:41:58 +1300

    Add to do items

---
+++ app/src/main/java/com/stuxo/notify/controller/ToDoListController.java
@@ -0,0 +1,7 @@
+package com.stuxo.notify.controller;
+
+/**
+ * Created by stu on 13/11/14.
+ */
+public class ToDoListController {
+}
--- app/src/main/java/com/stuxo/notify/model/ToDoItem.java
+++ app/src/main/java/com/stuxo/notify/model/ToDoItem.java
@@ -16,4 +16,37 @@ public class ToDoItem{
         alarmTime = null;
     }
 
+    public void setIsComplete(boolean isComplete){
+        this.isComplete = isComplete;
+    }
+
+    public boolean getIsComplete(){
+        return isComplete;
+    }
+
+    public int getId(){
+        return Id;
+    }
+
+    public String getText(){
+        return text;
+    }
+//
+//    public void createNotification(){
+//                        Intent intent = new Intent();
+//                intent.setAction(KEY_DONE);
+//
+//                //on press go to activity with list of activities
+//        //notifications only appear when due
+//
+//        //Added activities go on the main screen as list view
+//                intent.setClass(getApplicationContext(), NotificationActionReceiver.class);
+//
+//                PendingIntent doneIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, 0);
+//
+//                Notification notification = createBasicNotification("Don't forget!", txtNotificationDescription.getText().toString(), doneIntent);
+//
+//                notification.extras.putInt("Id", temp);
+//                displayNotification(notification, temp);
+//    }
 }
